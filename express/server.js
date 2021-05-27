3162e7ab65931d6641b8aeb440177cb8a101402c
ideazhao <420750281@qq.com>
2021-05-27 23:52:55 +0800

    简陋的express-get请求实现

--- express/server.js
+++ express/server.js
@@ -1,10 +1,18 @@
 // 引入express
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
 // 创建app
 const app = express()
 //处理请求
 app.get('/',(req,res)=>{
     res.send('欢迎使用express框架搭建服务器！')
+})
+app.get('/register',(req,res)=>{
+    let filePath = path.join(__dirname,'register.html')
+    let content = fs.readFileSync(filePath,'utf-8')
+    res.send(content)
 })
 // 监听请求
 app.listen(3000,()=>{
