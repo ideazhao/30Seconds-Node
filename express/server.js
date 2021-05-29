1fcb78fe260eaf2474d47e2e69553486d1cc9b29
ideazhao <420750281@qq.com>
2021-05-29 20:57:03 +0800

    get方法获取参数

--- express/server.js
+++ express/server.js
@@ -9,10 +9,16 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send('欢迎使用express框架搭建服务器！')
 })
+//请求文件
 app.get('/register',(req,res)=>{
     let filePath = path.join(__dirname,'register.html')
     let content = fs.readFileSync(filePath,'utf-8')
     res.send(content)
+})
+// get获取参数  query属性是查询参数的对象
+app.get('/list',(req,res)=>{
+    let id = req.query.id
+    res.send(id)
 })
 // 监听请求
 app.listen(3000,()=>{
